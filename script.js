36cc872b7e2b687f67ce62961596c29c9adb96b0
Charles <charles.lamarque69@gmail.com>
2022-12-06 13:13:30 +0100

    Add round winning logic

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log("Hello, World! outside");
+function getComputerChoice() {
+    let random = Math.random();
+    if(random > .66) {
+        return 'Rock';
+    } else if ( random > .33) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = String(playerSelection).toLowerCase();
+    playerSelection = String(playerSelection).charAt(0).toUpperCase() + String(playerSelection).substring(1);
+    if (playerSelection === computerSelection)
+        return 'This is round is a draw';
+
+    if (isWinner(playerSelection, computerSelection))
+        return 'You Win! ' + playerSelection + ' beats ' + computerSelection;
+    else
+        return 'You Lose! ' + computerSelection + ' beats ' + playerSelection;  
+}
+
+function isWinner(playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case 'Rock':
+            if (computerSelection === 'Paper')
+                return false;
+            else
+                return true;
+        break;
+
+        case 'Paper':
+                if (computerSelection === 'Scissors')
+                return false;
+            else
+                return true;
+        break;
+
+        case 'Scissors':
+                if (computerSelection === 'Rock')
+                return false;
+            else
+                return true;
+        break;
+    }
+}
