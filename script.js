ba7bd9c3c8521356df0e943d00674d5fdad1d6fd
Charles Lamarque <charles.lamarque69@gmail.com>
2022-12-08 16:51:26 +0100

    Add UI for round selection and results

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('Rock, Paper or Scissors?');
-        let computerSelection = getComputerChoice();
-        let result = playRound(playerSelection, computerSelection);
-        console.log(result);
-    }
-}
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => button.addEventListener('click',
+     () => playRound(button.className, getComputerChoice())));
+
+const result = document.querySelector('.results');
 
 function getComputerChoice() {
     let random = Math.random();
     if(random > .66) {
         return 'Rock';
-    } else if ( random > .33) {
+    } else if (random > .33) {
         return 'Paper';
     } else {
         return 'Scissors';
@@ -22,12 +19,11 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = String(playerSelection).toLowerCase();
     playerSelection = String(playerSelection).charAt(0).toUpperCase() + String(playerSelection).substring(1);
     if (playerSelection === computerSelection)
-        return 'This is round is a draw';
-
-    if (isWinner(playerSelection, computerSelection))
-        return 'You Win! ' + playerSelection + ' beats ' + computerSelection;
+        result.textContent = "This round is a draw.";
+    else if (isWinner(playerSelection, computerSelection))
+        result.textContent = 'You Win! ' + playerSelection + ' beats ' + computerSelection;
     else
-        return 'You Lose! ' + computerSelection + ' beats ' + playerSelection;  
+        result.textContent = 'You Lose! ' + computerSelection + ' beats ' + playerSelection;  
 }
 
 function isWinner(playerSelection, computerSelection) {
